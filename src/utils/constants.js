060308fbed71723e401dbd57abf1cbaac0d07b8e
Mariano Perez <perezmariano.pm@gmail.com>
2025-03-22 13:59:43 -0300

    camios en contants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,2 @@
-export const URL_API = 'http://localhost:3001/api/'
-export const URL_IMAGES = 'https://white-crab-914199.hostingersite.com/uploads/images/'
+export const URL_API = process.env.URL_API;
+export const URL_IMAGES = process.env.URL_IMAGES;
