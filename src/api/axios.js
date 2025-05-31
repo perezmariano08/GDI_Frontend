4d927a1aaaab02967709807947a62bfd26f021c7
Mariano Perez <perezmariano.pm@gmail.com>
2025-05-30 23:26:33 -0300

    cambio en variable api

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: "http://localhost:3001/api",
-    // baseURL: "https://apineumaticor.vercel.app/api",
+    // baseURL: "http://localhost:3001/api",
+    baseURL: "https://apigolesdeinstituto.vercel.app/api",
     headers: {
         'Content-Type': 'application/json'
     }
