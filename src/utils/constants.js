d7c87c6535f5382ed5d50e74e3f708170440c06c
Mariano Perez <perezmariano.pm@gmail.com>
2025-05-30 23:42:23 -0300

    cambio en variable constants

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-export const URL_API = 'http://localhost:3001/api/'
-// export const URL_API = 'https://apigolesdeinstituto.vercel.app/api/'
+// export const URL_API = 'http://localhost:3001/api/'
+export const URL_API = 'https://apigolesdeinstituto.vercel.app/api/'
 export const URL_IMAGES = 'https://white-crab-914199.hostingersite.com/uploads/images/'
 export const URL_VIDEOS = 'https://white-crab-914199.hostingersite.com/uploads/videos/'
